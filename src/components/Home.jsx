0e2dce0f35b2d15ef08140bfc2ce2195a84e47c8
Aditya Panjiyar <aditya.8mi@gmail.com>
2023-04-01 17:50:56 +0530

    Lecture commit time 2:09:55

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import vg from "../assets/2.webp";
+import{
+  AiFillGoogleCircle,
+  AiFillAmazonCircle,
+  AiFillYoutube,
+  AiFillInstagram
+} from "react-icons/ai";
 
 const Home = () => {
   return (
@@ -22,6 +28,62 @@ const Home = () => {
         </p>
     </div>
     </div>
+
+    <div className='home3' id="about">
+      <div>
+        <h1>Who we are?</h1>
+        <p>
+          kuch bhi aacha type karna hai apne maan se
+        </p>
+      </div>
+    </div>
+
+    {/* home4 start */}
+
+    <div className="home4" id="brands">
+        <div>
+          <h1>Brands</h1>
+
+          <article>
+            <div
+              style={{
+                animationDelay: "0.3s",
+              }}
+            >
+              <AiFillGoogleCircle />
+              <p>Google</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "0.5s",
+              }}
+            >
+              <AiFillAmazonCircle />
+              <p>Amazon</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "0.7s",
+              }}
+            >
+              <AiFillYoutube />
+              <p>Youtube</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "1s",
+              }}
+            >
+              <AiFillInstagram />
+              <p>Instagram</p>
+            </div>
+          </article>
+        </div>
+      </div>
+    {/* home 4 nds */}
     
     </>
 
